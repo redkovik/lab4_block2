c75532c8e822537a6709e94c39876dc49d55cdb0
redkovik <bronikov.denys1123@vu.cdu.edu.ua>
2024-06-23 16:30:53 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Text;
+namespace Lab4
+{
+    class Program
+    {
+        static List<Student> ReadData(string fileName)
+        {
+            List<Student> students = [];
+            try
+            {
+                StreamReader reader = new(fileName);
+                string? line;
+                while ((line = reader.ReadLine()) != null)
+                    students.Add(new(line));
+            }
+            catch (IOException e) { Console.WriteLine($"Error: {e.Message}"); }
+            return students;
+        }
+
+        static void Var4(List<Student> students)
+        {
+            Console.WriteLine("Імена студентів жінок з відмінними оцінками з інформатики:");
+            foreach (var student in students)
+            {
+                if (student.Marks[2] == '5' && (student.sex == 'F' || student.sex == 'Ж'))
+                {
+                    Console.WriteLine($"{student.surName} {student.firstName} {student.patronymic}");
+                }
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = UTF8Encoding.UTF8;
+            List<Student> students = ReadData("../../../input.txt");
+            Var4(students);
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,36 @@
+﻿namespace Lab4
+{
+    public struct Student
+    {
+        public string surName;
+        public string firstName;
+        public string patronymic;
+        public char sex;
+        public string dateOfBirth;
+        public char[] Marks;
+        public int scholarship;
+
+        public override readonly string ToString()
+        {
+            return surName + " " + firstName + " " + patronymic + " " + dateOfBirth;
+        }
+        public Student(string line)
+        {
+            string[] fields = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            surName = fields[0];
+            firstName = fields[1];
+            patronymic = fields[2];
+            sex = char.Parse(fields[3]);
+            dateOfBirth = fields[4];
+            Marks = [
+        // математика
+        char.Parse(fields[5]),
+        // фізика
+        char.Parse(fields[6]),
+        // інформатика
+        char.Parse(fields[7])
+            ];
+            scholarship = int.Parse(fields[8]);
+        }
+    }
+}
